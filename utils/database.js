cd7f68748b983fd71a485b4c98f4d368e27e481f
Devvrat Mishra <devvrat.mishra78@gmail.com>
2021-08-09 16:41:48 +0530

    db

--- utils/database.js
+++ utils/database.js
@@ -1,1 +1,4 @@
 // this module will take care of b3/codbot database connection
+const mysql = require('promise-mysql')
+const { exit } = require('process')
+const ErrorHandler = require.main.require('./src/errorhandler')
