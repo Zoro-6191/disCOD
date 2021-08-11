a011f327589241bd9ccf61c9c6f9e53b4c724e92
BuildTools <unconfigured@null.spigotmc.org>
2021-08-11 14:11:24 +0530

    rcon init

--- utils/rcontool.js
+++ utils/rcontool.js
@@ -1,2 +1,14 @@
-// this module will take care of UDP connection to cod4 server
-const ErrorHandler = require.main.require('./src/errorhandler')
+// this file takes care of all rcon command sending related stuff
+// TO-DO: exception handling
+
+module.exports.initRcon = async function()
+{
+    const { createRconCommands } = require( '@arbytez/cod4-rcon-commands' )
+	const server = require.main.require('./conf').mainconfig.server
+
+    // TO-DO: check authenticity of config entries
+
+    module.exports.rcontool = createRconCommands( server.rcon_ip, parseInt(server.port), server.rcon_password )
+    
+    console.log("Initialized: Rcon UDP JS Library by arbytez")
+}
